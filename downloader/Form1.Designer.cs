24bebcd02369915cb2018cd932ccb34ab71691df
Duoslow <heniugur@gmail.com>
2020-06-29 11:59:46 +0300

    version 0.2

--- downloader/Form1.Designer.cs
+++ downloader/Form1.Designer.cs
@@ -28,13 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelPerc = new System.Windows.Forms.Label();
+            this.labelDownloaded = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.file = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(155, 184);
+            this.labelSpeed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(18, 20);
+            this.labelSpeed.TabIndex = 2;
+            this.labelSpeed.Text = "?";
+            // 
+            // labelPerc
+            // 
+            this.labelPerc.AutoSize = true;
+            this.labelPerc.Location = new System.Drawing.Point(155, 218);
+            this.labelPerc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPerc.Name = "labelPerc";
+            this.labelPerc.Size = new System.Drawing.Size(18, 20);
+            this.labelPerc.TabIndex = 3;
+            this.labelPerc.Text = "?";
+            // 
+            // labelDownloaded
+            // 
+            this.labelDownloaded.AutoSize = true;
+            this.labelDownloaded.Location = new System.Drawing.Point(155, 253);
+            this.labelDownloaded.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDownloaded.Name = "labelDownloaded";
+            this.labelDownloaded.Size = new System.Drawing.Size(18, 20);
+            this.labelDownloaded.TabIndex = 4;
+            this.labelDownloaded.Text = "?";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 284);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(745, 35);
+            this.progressBar.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 18);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 184);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Download Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 218);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Download Percent";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 253);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Downloaded";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "File Name";
+            // 
+            // file
+            // 
+            this.file.AutoSize = true;
+            this.file.Location = new System.Drawing.Point(155, 150);
+            this.file.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(18, 20);
+            this.file.TabIndex = 11;
+            this.file.Text = "?";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(526, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 24);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Credential";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.username);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(526, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(92, 53);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(100, 26);
+            this.password.TabIndex = 20;
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(93, 21);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 26);
+            this.username.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(771, 333);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.file);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelDownloaded);
+            this.Controls.Add(this.labelPerc);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "UUR Download Manager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelPerc;
+        private System.Windows.Forms.Label labelDownloaded;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label file;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- downloader/Form1.cs
+++ downloader/Form1.cs
@@ -2,19 +2,148 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace downloader
 {
     public partial class Form1 : Form
     {
+
+        WebClient webClient;
+        Stopwatch sw = new Stopwatch();
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        public void hee()
+        {
+            textBox1.Text = "";
+            file.Text = "";
+            labelSpeed.Text = "";
+            labelPerc.Text = "";
+            labelDownloaded.Text = "";
+            progressBar.Value = 0;
+        }
+        public Uri URL;
+        public void DownloadFile(string urlAddress, string location)
+        {
+           
+                   using (webClient = new WebClient())
+                   {
+                       webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                       webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                Uri address = new Uri(urlAddress);
+                if (address.Scheme == Uri.UriSchemeHttp)
+                {
+                    Console.WriteLine("HTTP");
+                    if (checkBox1.Checked)
+                    {
+                        webClient.Credentials = new NetworkCredential(username.Text, password.Text);
+                    }
+                    URL = urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+                }
+                if (address.Scheme == Uri.UriSchemeHttps)
+                {
+                    
+                    Console.WriteLine("Https");
+                    if (checkBox1.Checked)
+                    {
+                        webClient.Credentials = new NetworkCredential(username.Text, password.Text);
+                    }
+                    URL = urlAddress.StartsWith("https://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("https://" + urlAddress);
+                }
+                if (address.Scheme == Uri.UriSchemeFtp)
+                {
+                    Console.WriteLine("ftp");
+                    if (checkBox1.Checked)
+                    {
+                        webClient.Credentials = new NetworkCredential(username.Text, password.Text);
+                    }
+                    
+                    URL = urlAddress.StartsWith("ftp://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("ftp://" + urlAddress);
+                }
+                sw.Start();
+               
+                       try
+                       {
+                           webClient.DownloadFileAsync(URL, location);
+                       }
+                       catch (Exception ex)
+                       {
+                           MessageBox.Show(ex.Message);
+                       }
+                   }
+        }
+
+
+       
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            labelSpeed.Text = string.Format("{0} mb/s", (e.BytesReceived / 1024d / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+            progressBar.Value = e.ProgressPercentage;
+            labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+            labelDownloaded.Text = string.Format("{0} MB / {1} MB",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+
+        }
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            sw.Reset();
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.");
+                File.Delete(za);
+                hee();
+            }
+            else
+            {
+                MessageBox.Show("Download completed!");
+                Process.Start("explorer.exe", "/select," + za + "");
+                hee();
+            }
+        }
+        public string za;
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            var lastPart = textBox1.Text.Split('/').Where(x => !string.IsNullOrWhiteSpace(x)).LastOrDefault();
+            file.Text = lastPart;
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                 za = folderBrowserDialog1.SelectedPath+"\\"+lastPart;
+                DownloadFile(textBox1.Text, za);
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webClient.CancelAsync();
+            hee();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                groupBox1.Visible = true;
+            }
+            else
+            {
+                groupBox1.Visible = false;
+            }
         }
     }
 }
